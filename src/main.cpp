fbb4b40734674ccec17e5a2bf0f41d0d3625b3b6
Juan Gonzalez <j-gonzalez@email.de>
2024-08-25 11:04:09 +0700

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+//  Copyright (c) 2018-2024 Juan R. González
+//
+//  This file is part of Docklight.
+//
+//  Docklight is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Docklight is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 #include <gtkmm/application.h>
@@ -25,7 +41,6 @@ int main(int argc, char *argv[])
     XInitThreads();
 
     // The initialization code:
-
     // Set up the user current locale.
     setlocale(LC_ALL, "");
     g_print("Initialize gettext\n");
