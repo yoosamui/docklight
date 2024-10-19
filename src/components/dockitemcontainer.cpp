009bb023296525e71ef2e3ebad06ec93a3303533
Juan Gonzalez <j-gonzalez@email.de>
2024-10-19 23:44:22 +0700

    clean code

--- src/components/dockitemcontainer.cpp
+++ src/components/dockitemcontainer.cpp
@@ -79,10 +79,6 @@ namespace docklight
     template <typename T>
     bool DockItemContainer::exist(gulong xid, bool deep)
     {
-        if (xid == 0) {
-            exit(1);
-        }
-
         std::shared_ptr<T> dockitem;
 
         for (auto it = m_dockitems.begin(); it != m_dockitems.end(); it++) {
