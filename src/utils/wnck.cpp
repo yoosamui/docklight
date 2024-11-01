ebf7e79d7f06ee6ba11f9f2433bc37217b159baa
Juan Gonzalez <j-gonzalez@email.de>
2024-11-01 22:09:14 +0700

    modify window selector code

--- src/utils/wnck.cpp
+++ src/utils/wnck.cpp
@@ -154,7 +154,7 @@ namespace docklight
                             WnckWorkspace* ws = wnck_window_get_workspace(w);
                             if (wnck_workspace_get_number(ws) != current_ws_number) continue;
 
-                            //    focus_window(w, event_time);
+                            focus_window(w, event_time);
                         }
                     }
 
@@ -164,10 +164,8 @@ namespace docklight
 
             for (auto& window : window_list) {
                 if (window) {
-                    ;
+                    focus_window(window, event_time);
                 }
-                //   focus_window(window, event_time);
-                g_print("FOCUS\n");
             }
         }
 
@@ -192,9 +190,9 @@ namespace docklight
             }
 
             focus_window(window, event_time);
-            // if (!wnck_window_is_active(window)) {
-            // wnck_window_activate(window, ct);
-            //}
+            if (!wnck_window_is_active(window)) {
+                wnck_window_activate(window, event_time);
+            }
         }
 
         void bring_above_window(WnckWindow* window)
