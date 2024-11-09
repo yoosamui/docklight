0c86020ea4dcd6ed604e024af48cf7047ee704bb
Juan Gonzalez <j-gonzalez@email.de>
2024-11-10 06:58:42 +0700

    preview best i can do for now

--- src/components/panelpreview.cpp
+++ src/components/panelpreview.cpp
@@ -275,7 +275,7 @@ namespace docklight
                 }
                 if (wnck_window_is_minimized(window)) {
                     wnck::unminimize(window);
-                    std::this_thread::sleep_for(std::chrono::milliseconds(140));
+                    std::this_thread::sleep_for(std::chrono::milliseconds(40));
                 }
 
                 pixbuf::get_window_image(xid, m_image);
