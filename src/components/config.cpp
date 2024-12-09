e0790805e84c75970cf00bda5c376c69f56515ba
Juan Gonzalez <j-gonzalez@email.de>
2024-12-09 18:14:43 +0700

    looks good

--- src/components/config.cpp
+++ src/components/config.cpp
@@ -158,8 +158,6 @@ namespace docklight
 
     dock_alignment_t Configuration::get_dock_alignment() const
     {
-        if (m_alignment != dock_alignment_t::fill) exit(1);
-
         return m_alignment;
     }
 
