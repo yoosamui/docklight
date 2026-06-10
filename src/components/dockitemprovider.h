349b192afd606024d112219e59a8d4dda648afa9
Juan Gonzalez <j-gonzalez@email.de>
2026-06-10 13:54:06 +0700

    importand change in dockitemprovider.cpp

--- src/components/dockitemprovider.h
+++ src/components/dockitemprovider.h
@@ -38,108 +38,115 @@
 namespace docklight
 {
 
-    enum window_action_t { OPEN, CLOSE, UPDATE, WORKSPACE };
-    typedef sigc::signal<void, window_action_t, glong> type_signal_update;
+  enum window_action_t
+  {
+    OPEN,
+    CLOSE,
+    UPDATE,
+    WORKSPACE
+  };
+  typedef sigc::signal<void, window_action_t, glong> type_signal_update;
 
-    class DockItemProvider : public Glib::Object
+  class DockItemProvider : public Glib::Object
+  {
+    typedef struct
     {
-        typedef struct {
-            char name[60];
-            char title[60];
-            char group[60];
-            char instance[60];
-            char locale[20];
-            char comment[512];
-            char desktop_file[512];
-            char icon_name[128];
-            guint8 pixbuff[128 * 128 * 3] = {0};  //  128 x 128 x 3 icon max size
+      char name[60];
+      char title[60];
+      char group[60];
+      char instance[60];
+      char locale[20];
+      char comment[512];
+      char desktop_file[512];
+      char icon_name[128];
+      guint8 pixbuff[128 * 128 * 3] = {0}; //  128 x 128 x 3 icon max size
 
-        } attach_rec_t;
+    } attach_rec_t;
 
-      public:
-        DockItemProvider();
-        virtual ~DockItemProvider();
+  public:
+    DockItemProvider();
+    virtual ~DockItemProvider();
 
-        // void request_update_signal();
-        type_signal_update signal_update();
+    // void request_update_signal();
+    type_signal_update signal_update();
 
-        guint required_size(guint additional_size);
-        int remove(gulong xid);
+    guint required_size(guint additional_size);
+    int remove(gulong xid);
 
-        bool exist(gulong xid);
-        bool insert(WnckWindow* window);
-        bool get_dockitem_by_index(guint index, std::shared_ptr<DockItemIcon>& dockitem);
-        bool get_dockitem_by_xid(gulong xid, std::shared_ptr<DockItemIcon>& dockitem);
-        bool attach(guint index, bool attach);
+    bool exist(gulong xid);
+    bool insert(WnckWindow *window);
+    bool get_dockitem_by_index(guint index, std::shared_ptr<DockItemIcon> &dockitem);
+    bool get_dockitem_by_xid(gulong xid, std::shared_ptr<DockItemIcon> &dockitem);
+    bool attach(guint index, bool attach);
 
-        std::vector<std::shared_ptr<DockItemIcon>>& data();
+    std::vector<std::shared_ptr<DockItemIcon>> &data();
 
-        guint count();
-        bool save();
+    guint count();
+    bool save();
 
-        Glib::RefPtr<Gdk::Pixbuf> get_window_image(gulong xid);
-        void set_window_image(WnckWindow* window, bool initial = false);
-        void set_window_image(gulong xid, Glib::RefPtr<Gdk::Pixbuf> image);
+    Glib::RefPtr<Gdk::Pixbuf> get_window_image(gulong xid);
+    void set_window_image(WnckWindow *window, bool initial = false);
+    void set_window_image(gulong xid, Glib::RefPtr<Gdk::Pixbuf> image);
 
-        //        void set_window_image_reload(WnckWindow* window, bool initial);
+    //        void set_window_image_reload(WnckWindow* window, bool initial);
 
-        void emit_update();
-        void workspace_change();
+    void emit_update();
+    void workspace_change();
 
-        void swap(int source, int dest);
-        void drop(int soruce, int dest);
+    void swap(int source, int dest);
+    void drop(int soruce, int dest);
 
-      private:
-        sigc::connection m_sigc;
-        WnckWindow* m_wnckwindow = nullptr;
+  private:
+    sigc::connection m_sigc;
+    WnckWindow *m_wnckwindow = nullptr;
 
-        void on_theme_changed();
-        bool on_timeout();
+    void on_theme_changed();
+    bool on_timeout();
 
-        bool get_window_icon(GdkPixbuf* gdkpixbuf, Glib::RefPtr<Gdk::Pixbuf>& pixbuf);
-        bool get_theme_icon(guint xid, Glib::RefPtr<Gdk::Pixbuf>& pixbuf);
-        bool get_theme_icon(guint xid, Glib::RefPtr<Gdk::Pixbuf>& pixbuf, Glib::ustring& title_name,
-                            Glib::ustring& desktop_file, Glib::ustring& icon_name);
+    bool get_window_icon(GdkPixbuf *gdkpixbuf, Glib::RefPtr<Gdk::Pixbuf> &pixbuf);
+    bool get_theme_icon(guint xid, Glib::RefPtr<Gdk::Pixbuf> &pixbuf);
+    bool get_theme_icon(guint xid, Glib::RefPtr<Gdk::Pixbuf> &pixbuf, Glib::ustring &title_name,
+                        Glib::ustring &desktop_file, Glib::ustring &icon_name);
 
-        bool createFromDesktopFile(gulong xid, GdkPixbuf* gdkpixbuf, Glib::ustring instance_name,
-                                   std::string group_name, Glib::ustring window_name,
-                                   Glib::ustring window_icon_name, bool icon_is_fallback,
-                                   WnckWindowType wintype);
+    bool createFromDesktopFile(gulong xid, GdkPixbuf *gdkpixbuf, Glib::ustring instance_name,
+                               std::string group_name, Glib::ustring window_name,
+                               Glib::ustring window_icon_name, bool icon_is_fallback,
+                               WnckWindowType wintype);
 
-        bool createFromWindow(gulong xid, GdkPixbuf* gdkpixbuf, Glib::ustring instance_name,
-                              std::string groupname, Glib::ustring window_name,
-                              Glib::ustring window_icon_name, bool icon_is_fallback,
-                              WnckWindowType wintype);
+    bool createFromWindow(gulong xid, GdkPixbuf *gdkpixbuf, Glib::ustring instance_name,
+                          std::string groupname, Glib::ustring window_name,
+                          Glib::ustring window_icon_name, bool icon_is_fallback,
+                          WnckWindowType wintype);
 
-        bool insert(gulong xid, GdkPixbuf* gdkpixbuf, const Glib::ustring instance_name,
-                    std::string group_name, const Glib::ustring window_name,
-                    const Glib::ustring window_icon_name, bool icon_is_fallback,
-                    WnckWindowType wintype);
+    bool insert(gulong xid, GdkPixbuf *gdkpixbuf, const Glib::ustring instance_name,
+                std::string group_name, const Glib::ustring window_name,
+                const Glib::ustring window_icon_name, bool icon_is_fallback,
+                WnckWindowType wintype);
 
-        Glib::ustring get_config_filepath();
+    Glib::ustring get_config_filepath();
 
-        void fill_window_image(WnckWindow* window);
-        bool capture_window_image(gint32 xid);
+    void fill_window_image(WnckWindow *window);
+    bool capture_window_image(gint32 xid, WnckWindow *window);
 
-        void scan_initial_windows();
-        bool load();
+    void scan_initial_windows();
+    bool load();
 
-      private:
-        sigc::connection m_sigc_timer;
-        type_signal_update m_signal_update;
+  private:
+    sigc::connection m_sigc_timer;
+    type_signal_update m_signal_update;
 
-        DockItemContainer m_container;
-        BamfMatcher* m_matcher = nullptr;
+    DockItemContainer m_container;
+    BamfMatcher *m_matcher = nullptr;
 
-        bool m_startup_time_set = false;
-        bool m_startup_allow_window_scan = false;
-        std::vector<std::pair<int, WnckWindow*>> m_windows_loaded;
-        std::map<gulong, Glib::RefPtr<Gdk::Pixbuf>> m_window_images;
-        std::mutex m_mutex;
-        std::shared_ptr<DockItemIcon> m_home_dockitem;
-    };
+    bool m_startup_time_set = false;
+    bool m_startup_allow_window_scan = false;
+    std::vector<std::pair<int, WnckWindow *>> m_windows_loaded;
+    std::map<gulong, Glib::RefPtr<Gdk::Pixbuf>> m_window_images;
+    std::mutex m_mutex;
+    std::shared_ptr<DockItemIcon> m_home_dockitem;
+  };
 
-    Glib::RefPtr<DockItemProvider> create_provider();
-    Glib::RefPtr<DockItemProvider> Provider();
+  Glib::RefPtr<DockItemProvider> create_provider();
+  Glib::RefPtr<DockItemProvider> Provider();
 
-}  // namespace docklight
+} // namespace docklight
